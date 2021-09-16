4bd8b33c478155fb4f1a8e472d9a0c983a6508bc
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-16 10:20:53 -0400

    updated the view and controller so they can interact

--- src/main/java/com/example/f21comp1011gcw2/CreateCameraViewController.java
+++ src/main/java/com/example/f21comp1011gcw2/CreateCameraViewController.java
@@ -1,4 +1,33 @@
 package com.example.f21comp1011gcw2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class CreateCameraViewController {
+
+    @FXML
+    private ComboBox<?> makeComboBox;
+
+    @FXML
+    private TextField modelTextField;
+
+    @FXML
+    private Spinner<?> mpSpinner;
+
+    @FXML
+    private TextField priceTextField;
+
+    @FXML
+    private CheckBox digitalCheckBox;
+
+    @FXML
+    private CheckBox mirrorlessCheckBox;
+
+    @FXML
+    private Label msgLabel;
+
 }
